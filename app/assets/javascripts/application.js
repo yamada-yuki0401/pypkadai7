ce4235a052b6475577d92c264fc92607d6784f60
EC2 Default User <ec2-user@ip-172-31-64-181.ec2.internal>
2021-03-06 01:48:41 +0000

    oyokadai7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,14 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery
+
+$(function() {
+  return $('#user_postcode').jpostal({
+    postcode: ['#user_postcode'],
+    address: {
+      '#user_prefecture_code': '%3',
+      '#user_address_city': '%4',
+      '#user_address_street': '%5%6%7',
+    },
+  });
+});
